9e2eee20e0c5ab3bb7a418afe3a127118d74a69e
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-01-20 22:43:49 +0300

    changed workdir

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd $GOPATH
-docker run --rm -it --privileged -v $(pwd):/go/src -v //var/run/docker.sock:/var/run/docker.sock operator-sdk
+docker run --rm -it --privileged -v $GOPATH:/go/src -v //var/run/docker.sock:/var/run/docker.sock operator-sdk
