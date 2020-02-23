85d002bafc35600e4c739516a391eb793b1b138c
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-02-23 20:30:35 +0000

    small fixes

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -v $GOPATH:/go/src -v //var/run/docker.sock:/var/run/docker.sock operator-sdk
+docker run --rm -it -v $(pwd):/go/src -v //var/run/docker.sock:/var/run/docker.sock f0m41h4u7/operator-sdk-image
